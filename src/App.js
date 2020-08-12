15f6c095c14f8609eac6b70bcfdec8944183b471
= <=>
2020-08-12 15:16:56 +0300

    state of the app is managed via component state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+
+
 
 class App extends Component {
-    render() {
-      return (
-        <div>
-          Hello World
-        </div>
-      )
-    }
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
   }
 
-  export default App;
+  removeContact = (contact) => {
+    this.setState( (prevState) => ({
+      contacts: prevState.contacts.filter ( (c) => {
+        return c.id !== contact
+      })
+    }))
+  }
+
+  render() {
+    return (
+      <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
+    )
+  }
+}
+
+export default App;
---
+++ src/ListContacts.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+class ListContacts extends Component {
+
+    state = {
+        query: ''
+    }
+
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    updateQuery = (query) => {
+        this.setState({
+            query: query.trim()
+        })
+    }
+
+    resetQuery = () => {
+        this.setState({
+            query: ''
+        })
+    } 
+
+    render() {
+
+        const {contacts, onDeleteContact} = this.props;
+        const {query} = this.state;
+
+        let showingContacts
+        if(query){
+            const match = new RegExp(escapeRegExp(query), 'i')
+            showingContacts = contacts.filter((c) => match.test(c.name))
+        }else{
+            showingContacts = contacts
+        }
+        showingContacts.sort(sortBy('name'))
+        return (
+            
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input
+                        type="text"
+                        className="search-contacts"
+                        placeholder="search contacts"
+                        value={this.state.query}
+                        onChange={(event) => { this.updateQuery(event.target.value) }}
+                    />
+                </div>
+                {showingContacts.length !== contacts.length && (
+                    <div className="showing-contacts">
+                        <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+                        <button onClick={this.resetQuery}>Show all</button>
+                    </div>
+                )}
+                <ol className="contact-list">
+                    {showingContacts.map(contact => {
+                        return (<li className="contact-list-item">
+                            <img className="contact-avatar" src={contact.avatarURL} alt="profile" />
+                            <div className="contact-details">
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => { onDeleteContact(contact.id) }} className="contact-remove">
+                                Remove
+                           </button>
+                        </li>)
+                    })}
+                </ol>
+            </div>
+        )
+    }
+}
+
+
+export default ListContacts;
